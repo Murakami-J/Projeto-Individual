ee3dc8656ffec010ded8ae175f35114392d84d3a
Murakami-J <juliana.oshikawa@sptech.school>
2024-05-24 15:58:21 -0300

    Arrumando o banco de dados

--- site-sptaiko/src/models/grupoModel.js
+++ site-sptaiko/src/models/grupoModel.js
@@ -4,17 +4,18 @@ function listarGrupo() {
 
     console.log("ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
     var instrucaoSql = `
-        SELECT 
-            g.nome,
+        SELECT g.nome,
             g.descricao,
-            g.qtdCurtida,
-            g.qtdComentario,
-            g.fkEndereco,
-            e.rua,
-            e.CEP,
-            e.numero,
-            e.complemento
-        FROM Grupo as g JOIN Endereco as e ON fkEndereco = idEndereco;
+            g.email,
+            g.instagram,
+            g.facebook,
+            concat(t.ddd, ' ', t.prefixo, '-', t.sufixo) as telefone,
+            concat(e.rua, ', ',
+            e.numero) as endereco,
+            e.CEP
+            FROM Grupo as g
+            JOIN Endereco as e ON e.fkGrupo = idGrupo
+            JOIN Telefone as t ON t.fkGrupo = idGrupo;
     `;
 
     // WHERE g.idGrupo = 1
